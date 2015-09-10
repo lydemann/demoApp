4effb77154be220deb730d2a2d2b260231f9e330
Christian Lüdemann <clu@it-minds.dk>
2015-09-10 10:10:44 +0200

    added calculator and calculator test

---
+++ demoApp/UnitTestProject1/CalculatorUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorUnitTest
+    {
+        [TestMethod]
+        public void Assert2plus3is5()
+        {
+            var UUT = new Calculator();
+            var result = UUT.AddTwoNumbers(2, 3);
+            Assert.AreEqual(result,5);
+        }
+    }
+}
